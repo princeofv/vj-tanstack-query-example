8832090333aad643329451009740098ce54f93eb
Vijay <vjwarboy13@gmail.com>
2024-12-25 19:16:08 +0530

    dev tool updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './globals.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -9,7 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        <QueryClientProvider client={queryClient}>{children}
+           <ReactQueryDevtools initialIsOpen={false} /></QueryClientProvider>
+       
       </body>
     </html>
   );
